693d316a750cacafe4f590553b181d4939b91c99
Usamah Akram <usamah@techalchemy.co>
2024-05-01 12:45:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+t1 = document.getElementById("t1")
+setTimeout(()=>{
+    t1.style.display = "block ";
+},2000)
+t2 = document.getElementById("t2")
+setTimeout(()=>{
+    t2.style.display = "block";
+    h2.style.display = "none";
+},3000)
+t3 = document.getElementById("t3")
+setTimeout(()=>{
+    t3.style.display = "block";
+    h3.style.display = "none";
+},6500)
+t4 = document.getElementById("t4")
+setTimeout(()=>{
+    t4.style.display = "block";
+    h4.style.display = "none";
+},10000)
+t5 = document.getElementById("t5")
+setTimeout(()=>{
+    t5.style.display = "block";
+    h5.style.display = "none";
+},11000)
+t6 = document.getElementById("t6")
+setTimeout(()=>{
+    t6.style.display = "block";
+    h6.style.display = "none";
+},13500)
+t7 = document.getElementById("t7")
+setTimeout(()=>{
+    t7.style.display = "block";
+    h7.style.display = "none";
+},16500)
+t8 = document.getElementById("t8")
+setTimeout(()=>{
+    t8.style.display = "block";
+    h8.style.display = "none";
+},17500)
+t9 = document.getElementById("t9")
+setTimeout(()=>{
+    t9.style.display = "block";
+    h9.style.display = "none";
+},20500)
+h1 = document.getElementById("h1")
+setTimeout(()=>{
+    h1.style.display = "none";
+},1800)
+
+h2 = document.getElementById("h2")
+setTimeout(()=>{
+    h2.style.display = "block";
+},2000)
+h3 = document.getElementById("h3")
+setTimeout(()=>{
+    h3.style.display = "block";
+},3500)
+h4 = document.getElementById("h4")
+setTimeout(()=>{
+    h4.style.display = "block";
+},8500)
+h5 = document.getElementById("h5")
+setTimeout(()=>{
+    h5.style.display = "block";
+},10000)
+h6 = document.getElementById("h6")
+setTimeout(()=>{
+    h6.style.display = "block";
+},11500)
+h7 = document.getElementById("h7")
+setTimeout(()=>{
+    h7.style.display = "block";
+},13800)
+h8 = document.getElementById("h8")
+setTimeout(()=>{
+    h8.style.display = "block";
+},16800)
+h9 = document.getElementById("h9")
+setTimeout(()=>{
+    h9.style.display = "block";
+},17800)
