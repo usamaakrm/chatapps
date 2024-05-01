c4ec23b2c48ca3c95bc33d252da85f13d825d217
Usamah Akram <usamah@techalchemy.co>
2024-05-01 19:03:55 +0530

    changes done

--- script.js
+++ script.js
@@ -21,27 +21,13 @@ t5 = document.getElementById("t5")
 setTimeout(()=>{
     t5.style.display = "block";
     h5.style.display = "none";
-},11000)
+},14000)
 t6 = document.getElementById("t6")
 setTimeout(()=>{
     t6.style.display = "block";
     h6.style.display = "none";
-},13500)
-t7 = document.getElementById("t7")
-setTimeout(()=>{
-    t7.style.display = "block";
-    h7.style.display = "none";
-},16500)
-t8 = document.getElementById("t8")
-setTimeout(()=>{
-    t8.style.display = "block";
-    h8.style.display = "none";
-},17500)
-t9 = document.getElementById("t9")
-setTimeout(()=>{
-    t9.style.display = "block";
-    h9.style.display = "none";
-},20500)
+},15500)
+
 h1 = document.getElementById("h1")
 setTimeout(()=>{
     h1.style.display = "none";
@@ -62,20 +48,112 @@ setTimeout(()=>{
 h5 = document.getElementById("h5")
 setTimeout(()=>{
     h5.style.display = "block";
-},10000)
+},10500)
 h6 = document.getElementById("h6")
 setTimeout(()=>{
     h6.style.display = "block";
-},11500)
-h7 = document.getElementById("h7")
+},14500)
+
+
+function btn1(){
+    t7 = document.getElementById("t7")
+    setTimeout(()=>{
+        t7.style.display = "block";
+        h7.style.display = "none";
+    },2000)
+    t8 = document.getElementById("t8")
+    setTimeout(()=>{
+        t8.style.display = "block";
+        h8.style.display = "none";
+    },3000)
+
+    h7 = document.getElementById("h7")
 setTimeout(()=>{
     h7.style.display = "block";
-},13800)
+},500)
 h8 = document.getElementById("h8")
 setTimeout(()=>{
     h8.style.display = "block";
-},16800)
-h9 = document.getElementById("h9")
+},2400)
+}
+
+function btn2(){
+    t9 = document.getElementById("t9")
+    setTimeout(()=>{
+        t9.style.display = "block";
+        h9.style.display = "none";
+        // s1.style.display = "none";
+    },2000)
+    t10 = document.getElementById("t10")
+    setTimeout(()=>{
+        t10.style.display = "block";
+        h10.style.display = "none";
+    },5000)
+    t11 = document.getElementById("t11")
+    setTimeout(()=>{
+        t11.style.display = "block";
+        h11.style.display = "none";
+    },7000)
+    t12 = document.getElementById("t12")
+    setTimeout(()=>{
+        t12.style.display = "block";
+        h12.style.display = "none";
+    },7600)
+
+    h9 = document.getElementById("h9")
 setTimeout(()=>{
     h9.style.display = "block";
-},17800)
+},500)
+h10 = document.getElementById("h10")
+setTimeout(()=>{
+    h10.style.display = "block";
+},2400)
+h11 = document.getElementById("h11")
+setTimeout(()=>{
+    h11.style.display = "block";
+},5500)
+h12 = document.getElementById("h12")
+setTimeout(()=>{
+    h12.style.display = "block";
+},7200)
+}
+function btn3(){
+    t14 = document.getElementById("t14")
+    setTimeout(()=>{
+        t14.style.display = "block";
+        h14.style.display = "none";
+        // s1.style.display = "none";
+    },2000)
+    t15 = document.getElementById("t15")
+    setTimeout(()=>{
+        t15.style.display = "block";
+        h15.style.display = "none";
+    },5000)
+    t16 = document.getElementById("t16")
+    setTimeout(()=>{
+        t16.style.display = "block";
+        h16.style.display = "none";
+    },7000)
+    // t12 = document.getElementById("t12")
+    // setTimeout(()=>{
+    //     t12.style.display = "block";
+    //     h12.style.display = "none";
+    // },7600)
+
+    h14 = document.getElementById("h14")
+setTimeout(()=>{
+    h14.style.display = "block";
+},500)
+h15 = document.getElementById("h15")
+setTimeout(()=>{
+    h15.style.display = "block";
+},2400)
+h16 = document.getElementById("h16")
+setTimeout(()=>{
+    h16.style.display = "block";
+},5500)
+t17 = document.getElementById("t17")
+setTimeout(()=>{
+    t17.style.display = "block";
+},7200)
+}
